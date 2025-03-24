3438828b57758a3040e01f4276f93c164f83b6f6
NotTamable <153935250+NotTamable@users.noreply.github.com>
2025-03-24 20:08:13 +0000

    ellobello

---
+++ products/products.js
@@ -0,0 +1,29 @@
+const products = [
+    { name: "Astroz Hoodie - Green", price: "£10", },
+    { name: "Product2", price: "£20", },
+    { name: "Product3", price: "£30", }
+];
+
+document.addEventListener("DOMContentLoaded", function() {
+    const productsContainer = document.getElementById("products-container");
+    products.forEach(product => {
+        const productDiv = document.createElement("div");
+        productDiv.className = "product";
+
+        const productImage = document.createElement("img");
+        productImage.src = `/ProductImages/${product.name}.webp`;
+        productImage.alt = product.name;
+
+        const productName = document.createElement("h3");
+        productName.textContent = product.name;
+
+        const productPrice = document.createElement("p");
+        productPrice.textContent = product.price;
+
+        productDiv.appendChild(productImage);
+        productDiv.appendChild(productName);
+        productDiv.appendChild(productPrice);
+
+        productsContainer.appendChild(productDiv);
+    });
+});

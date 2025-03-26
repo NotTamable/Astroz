cd14165baad305f1c8738b9efeca345ec24f8ac9
NotTamable <153935250+NotTamable@users.noreply.github.com>
2025-03-26 09:27:22 +0000

    fiwwe

--- products/product/script.js
+++ products/product/script.js
@@ -20,7 +20,7 @@ if (product) {
     const priceContainer = productDetails.querySelector('.price-container');
 
     const productPriceExclVAT = priceContainer.querySelector('.excl-vat');
-    productPriceExclVAT.textContent = `Excl. VAT: £${(product.price * 0.8).toFixed(2)}`;
+    productPriceExclVAT.textContent = `£${(product.price * 0.8).toFixed(2)}`;
 
     const productPriceInclVAT = priceContainer.querySelector('.price');
     productPriceInclVAT.textContent = `£${product.price.toFixed(2)}incl. VAT`;
